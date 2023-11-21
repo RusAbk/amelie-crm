5d7b85f67e47b71f97e2ee29d0ff30cf6cee75d9
Ruslan Abkadirov <rusabk@yandex.ru>
2023-11-21 10:47:50 +0700

    additional lead field  migraton added

--- database/migrations/2023_11_21_033211_create_lead_additional_fields_table.php
+++ database/migrations/2023_11_21_033211_create_lead_additional_fields_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('lead_additional_fields', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('lead_id');
+            $table->string('title');
+            $table->string('value');
             $table->timestamps();
         });
     }
